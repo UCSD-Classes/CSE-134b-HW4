39880c3ad75cf62ef73734110fc5bdd9f870cb7e
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-02-25 13:56:04 -0800

    uncomment theme script from hw3

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,26 +1,26 @@
-// const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 
-// // Track the theme switch button 
+//Track the theme switch button 
 
-// function switchTheme(e) {
-//     if (e.target.checked) {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//         localStorage.setItem('theme', 'dark'); //add this
-//     }
-//     else {
-//         document.documentElement.setAttribute('data-theme', 'light');
-//         localStorage.setItem('theme', 'light'); //add this
-//     }    
-// }
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
 
-// toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener('change', switchTheme, false);
 
-// const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-// if (currentTheme) {
-//     document.documentElement.setAttribute('data-theme', currentTheme);
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
-//     if (currentTheme === 'dark') {
-//         toggleSwitch.checked = true;
-//     }
-// }
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
