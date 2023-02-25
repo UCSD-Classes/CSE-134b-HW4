976b59449a231c80c90fbf658e45f1af3d326d32
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-02-25 13:56:36 -0800

    cleared main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
-// let nav = false;
+import { showDialog } from './customdialog.js';
 
-// const handleClick = () => {
-//   nav = !nav;
-//   if (!nav) {
-//     document.querySelector('.hMenu i').classList.remove('fa-times');
-//     document.querySelector('.hMenu i').classList.add('fa-bars');
-//   } else {
-//     document.querySelector('.hMenu i').classList.remove('fa-bars');
-//     document.querySelector('.hMenu i').classList.add('fa-times');
-//   }
-// };
+let nav = false;
+
+const handleClick = () => {
+  nav = !nav;
+  if (!nav) {
+    document.querySelector('.hMenu i').classList.remove('fa-times');
+    document.querySelector('.hMenu i').classList.add('fa-bars');
+  } else {
+    document.querySelector('.hMenu i').classList.remove('fa-bars');
+    document.querySelector('.hMenu i').classList.add('fa-times');
+  }
+};
+
